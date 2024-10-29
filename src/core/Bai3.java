11b762e69f78a8fdb3e1806dd2151fe25504e17d
Vuong <vanvuong2005827@gmail.com>
2024-10-29 19:48:48 +0700

    up2

--- src/core/Bai3.java
+++ src/core/Bai3.java
@@ -1,4 +1,12 @@
 package core;
 
+import java.util.Scanner;
+
 public class Bai3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double x = sc.nextDouble();
+        long y = sc.nextLong();
+        System.out.println(Math.pow(x, y));
+    }
 }
